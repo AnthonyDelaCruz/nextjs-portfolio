51518d6aca9e89dff60562975591893826b5a3a6
Tony dela cruz <delacruztony.adc@gmail.com>
2020-07-11 03:21:30 +0800

    added initial styles and content to projects section

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,9 +1,34 @@
+import { FaLock } from "react-icons/fa";
+
 import Layout from "@components/Layout";
+import Navbar from "@components/Navbar";
+
+const projects = [
+  { label: "Portfolio", exists: true },
+  { label: "SplashPhotography", exists: true },
+  { label: "Coming Soon!", exists: false },
+  { label: "Coming Soon!", exists: false },
+  { label: "Coming Soon!", exists: false },
+];
 
 export default function Projects() {
   return (
     <Layout>
-      <h1>PROJECTS</h1>
+      <Navbar customClassName="projects__nav" />
+      <section className="page-projects">
+        <h1>Current existing projects.</h1>
+        <div className="projects">
+          {projects.map(({ label, exists }) => (
+            <div className="project__item">
+              <div className="item__details">
+                {!exists && <FaLock size={50} />}
+                <span>{label}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="projects__notif">More projects coming soon!</div>
+      </section>
     </Layout>
   );
 }
